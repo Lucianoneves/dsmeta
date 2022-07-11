e9c8bdcbcbdcf7d8a9e55bcaa2582350c078e5b2
Luciano Neves <neves87_@hotmail.com>
2022-07-11 12:11:48 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,19 @@
-import NotificationButton from './components/NotificationButton'
-function App() {
- return (
-  <>
-  <h1>Ola mundo</h1>
-  <NotificationButton/>
-  <NotificationButton/>
-  <NotificationButton/>
 
-  </>
- )
+import Header from "./components/Header";
+import SalesCard from "./components/SalesCard";
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+            <SalesCard/>
+          </div>
+        </section>
+      </main>
+    </>
+  );
 }
 
-export default App
+export default App;
